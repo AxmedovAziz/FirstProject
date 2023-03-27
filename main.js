22c35af0a6df0bf08e6bbaa425804cbe7335d19a
aziz axmdov <AxmdovAziz>
2023-03-27 09:20:13 +0500

    sardor

--- main.js
+++ main.js
@@ -1,6 +1,92 @@
-let input = document.querySelector("input")
-let head = document.querySelector("h1")
 
-function real(){
-    head.innerHTML = input.value
-}
+// let input = document.querySelector("input")
+// let head = document.querySelector("h1")
+
+// function real(){
+//     head.innerHTML = input.value
+// }let input = document.querySelector("input")
+// let head = document.querySelector("h1")
+
+// function real(){
+//     head.innerHTML = input.value
+// }let input = document.querySelector("input")
+// let head = document.querySelector("h1")
+
+// function real(){
+//     head.innerHTML = input.value
+// }let input = document.querySelector("input")
+// let head = document.querySelector("h1")
+
+// function real(){
+//     head.innerHTML = input.value
+// }let input = document.querySelector("input")
+// let head = document.querySelector("h1")
+
+// function real(){
+//     head.innerHTML = input.value
+// }let input = document.querySelector("input")
+// let head = document.querySelector("h1")
+
+// function real(){
+//     head.innerHTML = input.value
+// }let input = document.querySelector("input")
+// let head = document.querySelector("h1")
+
+// function real(){
+//     head.innerHTML = input.value
+// }let input = document.querySelector("input")
+// let head = document.querySelector("h1")
+
+// function real(){
+//     head.innerHTML = input.value
+// }let input = document.querySelector("input")
+// let head = document.querySelector("h1")
+
+// function real(){
+//     head.innerHTML = input.value
+// }let input = document.querySelector("input")
+// let head = document.querySelector("h1")
+
+// function real(){
+//     head.innerHTML = input.value
+// }let input = document.querySelector("input")
+// let head = document.querySelector("h1")
+
+// function real(){
+//     head.innerHTML = input.value
+// }let input = document.querySelector("input")
+// let head = document.querySelector("h1")
+
+// function real(){
+//     head.innerHTML = input.value
+// }let input = document.querySelector("input")
+// let head = document.querySelector("h1")
+
+// function real(){
+//     head.innerHTML = input.value
+// }let input = document.querySelector("input")
+// let head = document.querySelector("h1")
+
+// function real(){
+//     head.innerHTML = input.value
+// }let input = document.querySelector("input")
+// let head = document.querySelector("h1")
+
+// function real(){
+//     head.innerHTML = input.value
+// }let input = document.querySelector("input")
+// let head = document.querySelector("h1")
+
+// function real(){
+//     head.innerHTML = input.value
+// }let input = document.querySelector("input")
+// let head = document.querySelector("h1")
+
+// function real(){
+//     head.innerHTML = input.value
+// }let input = document.querySelector("input")
+// let head = document.querySelector("h1")
+
+// function real(){
+//     head.innerHTML = input.value
+// }
